741c745a627fe2486ca77eee1741a55710fbe473
natanlazzariniproenca <natan.lazzarini.proenca@escola.pr.gov.br>
2026-03-09 13:26:28 +0000

    introdução aos Arrays em JS

---
+++ arrays/array-time.js
@@ -0,0 +1,10 @@
+// Array: são agrupamentos de valores em forma de list
+// São indexados a partir de ZERO até o infinito
+// Podem ter valores homogêneos/heterogêneos
+// Possuem métodos (funções) próprias
+
+const time = ['Zé','Léo']
+// indeices      0        0
+
+console.log(time[1])
+console.log(time[32]) //undefined
