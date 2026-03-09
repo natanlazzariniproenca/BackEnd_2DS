1a5b59f07b2294d88e7836b462935d3d15a43cca
natanlazzariniproenca <natan.lazzarini.proenca@escola.pr.gov.br>
2026-03-09 13:29:45 +0000

    Aprendizaagem da codificação de Arrays

---
+++ condicionais/if-else.js
@@ -0,0 +1,16 @@
+// if, else if, else, ternário
+// >, <, >=, <=, !=
+// ||, &&
+
+let idade = 2
+if (idade >= 765) {
+    console.log("Entrada somente acompanhado")
+    console.log("Festa sugerida: baile dos velhos")
+}
+else if (idade >= 18 && idade <= 765) {
+    console.log("Entrada permitida")
+    console.log("Festa sugerida: Camburão da ROTA")
+} else {
+    console.log("Entrada ESTRITAMENTE PROIBIDA")
+    console.log("Festa sugerida: NENHUMA")
+}
---
+++ variaveis/var.pessoa.js
@@ -0,0 +1,14 @@
+let nome = "Bruce"
+let sobrenome= "Lee"
+const cpf = "407.193.659-09"
+const rg = "53.481.027-X"
+let idade = "32"
+let altura = "1.72"
+let peso = "65"
+console.log(nome)
+console.log(sobrenome)
+console.log(cpf)
+console.log(rg)
+console.log(idade)
+console.log(altura)
+console.log(peso)
