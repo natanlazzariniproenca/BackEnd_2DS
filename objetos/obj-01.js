2fca7d633ba27bc7138baab818adfc8f47419d86
natanlazzariniproenca <natan.lazzarini.proenca@escola.pr.gov.br>
2026-03-25 11:39:12 +0000

    Finalização do objeto JS

---
+++ objetos/obj-01.js
@@ -0,0 +1,18 @@
+//objetos são estruturas chave-valor
+//também chamadas "key-value"
+//objetos podem conter: arrays, string,
+//number, objects, functions...
+//possuem métodos próprios
+
+const jogador = {
+    nome: "Kaka",
+    gols: 237,
+    altura: 1.86,
+    peso: 82,
+    times: ['São Paulo', 'Milan', 'Real Madrid', 'Orlando City']
+}
+console.log(jogador.nome)
+console.log(jogador.gols)
+console.log(jogador.altura)
+console.log(jogador.peso)
+console.log(jogador.times[1])
---
+++ objetos/obj-02.js
@@ -0,0 +1,20 @@
+//A partir do objeto de exemplo crie:
+//Um objeto com o tema a sua escolha
+//Deve possuir no mínimo 6 propriedades
+//Imprima de um jeito amigavel:
+//Ex.: console.log("Cor do Veículo: " + carro.cor)
+
+const carro = {
+    marca: "Chevy Corvette",
+    modelo: "C7 ZR1",
+    hp: 755,
+    litragem: 6.2,
+    cilindros: 8,
+    ano: 2018
+}
+console.log("Corvette é uma submarca da Chevrolet, por isso Chevy" + carro.marca)
+console.log("O modelo ZR1 é o modelo mais potente do modelo C7, seguido pelo Z06" + carro.modelo)
+console.log("hp significa Horse Power, a tradução seria cavalos" + carro.hp)
+console.log("A litragem é um sistema de medidas usado pelo motor, quanto maior, mais potente é o carro" + carro.litragem)
+console.log("Os cilindros são responsáveis por maiores rpms, portanto, maior potência, são encontrados no cabeçote do motor" + carro.cilindros)
+console.log("O ano que esse modelo do carro foi fabricado" + carro.ano)
