0d27e2fe19d1e780ad68045da45fe35d91541424
natanlazzariniproenca <natan.lazzarini.proenca@escola.pr.gov.br>
2026-03-25 11:47:23 +0000

    Finalização dos objetos .js

--- objetos/obj-02.js
+++ objetos/obj-02.js
@@ -1,7 +1,7 @@
 //A partir do objeto de exemplo crie:
 //Um objeto com o tema a sua escolha
 //Deve possuir no mínimo 6 propriedades
-//Imprima de um jeito amigavel:
+//Imprima de um jeito amigavel:;
 //Ex.: console.log("Cor do Veículo: " + carro.cor)
 
 const carro = {
@@ -12,9 +12,9 @@ const carro = {
     cilindros: 8,
     ano: 2018
 }
-console.log("Corvette é uma submarca da Chevrolet, por isso Chevy" + carro.marca)
-console.log("O modelo ZR1 é o modelo mais potente do modelo C7, seguido pelo Z06" + carro.modelo)
-console.log("hp significa Horse Power, a tradução seria cavalos" + carro.hp)
-console.log("A litragem é um sistema de medidas usado pelo motor, quanto maior, mais potente é o carro" + carro.litragem)
-console.log("Os cilindros são responsáveis por maiores rpms, portanto, maior potência, são encontrados no cabeçote do motor" + carro.cilindros)
-console.log("O ano que esse modelo do carro foi fabricado" + carro.ano)
+console.log("Corvette é uma submarca da Chevrolet, por isso: " + carro.marca)
+console.log("O modelo ZR1 é o modelo mais potente do modelo C7, seguido pelo Z06: " + carro.modelo)
+console.log("hp significa Horse Power, a tradução seria cavalos: " + carro.hp)
+console.log("A litragem é um sistema de medidas usado pelo motor, quanto maior, mais potente é o carro: " + carro.litragem)
+console.log("Os cilindros são responsáveis por maiores rpms, são encontrados no cabeçote do motor: " + carro.cilindros)
+console.log("O ano que esse modelo do carro foi fabricado: " + carro.ano)
