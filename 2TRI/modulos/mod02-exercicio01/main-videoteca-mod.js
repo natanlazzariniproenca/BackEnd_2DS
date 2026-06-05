780411ea6e3dfe4eae84bb1193807d853b2eb8fb
natanlazzariniproenca <natan.lazzarini.proenca@escola.pr.gov.br>
2026-06-05 04:57:42 +0000

    Finalização do código da locadora

---
+++ 2TRI/modulos/mod02-exercicio01/main-videoteca-mod.js
@@ -0,0 +1,5 @@
+import {alugarFilme, precoFilme, avaliarFilme} from './videoteca-mod.js';
+
+console.log(alugarFilme('Senhor dos anéis', '5'))
+console.log(precoFilme('Gigantes de aço', '12,50'))
+console.log(avaliarFilme('Vingadores', '4,7'))
---
+++ 2TRI/modulos/mod02-exercicio01/videoteca-mod.js
@@ -0,0 +1,12 @@
+export function alugarFilme(titulo, tempo)
+{
+    return `O cliente alugou o filme ${titulo} por: ${tempo} dias`
+}
+    export function precoFilme(titulo, preco)
+{
+    return `O cliente alugou o filme ${titulo} por: R$ ${preco}`
+}
+    export function avaliarFilme(titulo, nota)
+{
+    return `O cliente avaliou o filme ${titulo} com: ${nota} estrelas`
+}
