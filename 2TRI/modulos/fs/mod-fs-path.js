551bfdea70ca2c1b05ee71c3ece9ad5897728437
natanlazzariniproenca <natan.lazzarini.proenca@escola.pr.gov.br>
2026-06-16 10:59:23 +0000

    Instalação do chalk

---
+++ 2TRI/modulos/fs/mod-fs-path.js
@@ -0,0 +1,14 @@
+
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk').default
+
+try {
+//              módulo          diretório      arquivo    codificação
+const arquivo = fs.readFileSync(path.join(__dirname, '/festa.md'), 'utf-8')
+console.log(chalk.green('Arquivo lido com sucesso!:'))
+console.log(arquivo)
+} catch (erro) {
+    console.log(chalk.red("Sentou na bonceca:"))
+    console.log(erro.message)
+}
