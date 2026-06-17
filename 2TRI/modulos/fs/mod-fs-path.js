1b574e61b04228802d079a9f8093547459c7f0e9
natanlazzariniproenca <natan.lazzarini.proenca@escola.pr.gov.br>
2026-06-17 11:39:09 +0000

    Finalizado tratamento de erros com chalk

--- 2TRI/modulos/fs/mod-fs-path.js
+++ 2TRI/modulos/fs/mod-fs-path.js
@@ -4,10 +4,11 @@ const chalk = require('chalk').default
 
 try {
 //              módulo          diretório      arquivo    codificação
-const arquivo = fs.readFileSync(path.join(__dirname, '/festa.md'), 'utf-8')
-console.log(chalk.green('Arquivo lido com sucesso!:'))
+const arquivo = fs.readFileSync(path.join(__dirname, '/festa.mda'), 'utf-8')
+console.log(chalk.bgGreen.Bold('Arquivo lido com sucesso!'))
 console.log(arquivo)
 } catch (erro) {
-    console.log(chalk.red("Sentou na bonceca:"))
+    console.log(chalk.bgYellowBright.bold("⚠️ Erro ao ler o arquivo!"))
+    console.log(chalk.bgRed.bold("Sentou na boneca"))
     console.log(erro.message)
 }
