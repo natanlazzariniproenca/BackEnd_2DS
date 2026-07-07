eb381d75c3d12ccd4c1387c2b456571acfc65eb7
natanlazzariniproenca <natan.lazzarini.proenca@escola.pr.gov.br>
2026-07-07 11:45:38 +0000

    Ultima alteração pré-férias

---
+++ 2TRI/modulos/fs/fs-intervalo.js
@@ -0,0 +1,8 @@
+//setTimeout(ms) - executa 1 vez
+setTimeout(function(){
+    console.log('Executou FPS 75')
+}, 3000)
+//setInterval(ms) - executa '?'  vezes
+setInterval(function(){
+    console.log('Executou FPS 75')
+}, 2000)
